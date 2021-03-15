e0970ca7c76917f891b97a81aca2b6ab7792c3d9
Daryl Kow <dk3154@columbia.edu>
2021-03-15 11:02:43 -0400

    added neural network)

--- lib/train_gbm.R
+++
@@ -1,9 +0,0 @@
-#load library
-if(!require("gbm")){
-  install.packages("gbm")
-}
-library(gbm)
-train_gbm = function(features,labels,n.trees,shrinkage,w=NULL){
-  model=gbm.fit(x=features, y=labels, distribution ="multinomial", w=w, n.trees=n.trees,interaction.depth=2,shrinkage=shrinkage, verbose=FALSE)
-return(model)
-}
