b785309b3f670fa4772ac76054dcf80aa7dd3a18
Peter Kwauk <60371559+GigaPK@users.noreply.github.com>
2021-03-15 22:25:43 -0400

    Fixed rf training

--- lib/fit_train_randomforest.R
+++ lib/fit_train_randomforest.R
@@ -1,13 +1,12 @@
-####################################################################
-### Optimize and Train a Random Forest Model  ######################
-####################################################################
+#######################################################
+### Train a Random Forest Model  ######################
+#######################################################
 
 
 
-rf_opt_tree <- function(feature_train, label_train, mtry, ntree){
+rf_train <- function(feature_train, label_train, mtry, ntree){
   label_train_rf <- as.factor(label_train)
-  model <- tuneRF(x = feature_train, y = label_train_rf, mtryStart = mtry, ntreeTry= ntree, stepFactor=2, improve=0.05,
-                      trace=TRUE, plot=FALSE, doBest=TRUE)
+  model <- randomForest(x = feature_train, y = label_train_rf, mtry =  mtry, ntree =ntree)
   return(model)
 }
 
