978a62f35c1d3ccd950e55f741f28b22ca593094
Peter Kwauk <60371559+GigaPK@users.noreply.github.com>
2021-03-16 22:29:05 -0400

    Update fit_train_xgboost.R

--- lib/fit_train_xgboost.R
+++ lib/fit_train_xgboost.R
@@ -3,8 +3,8 @@
 ### Train an XGBoost classification model with training features ###
 ####################################################################
 
-xgboost_train <- function(features, labels, params, rounds = 50){
-  model <- xgboost(data = features, label = labels, params = params, nrounds = rounds, objective = "binary:logistic")
+xgboost_train <- function(features, labels, params, rounds = 50, spec_weights){
+  model <- xgboost(data = features, label = labels, params = params, nrounds = rounds, objective = "binary:logistic", weight = spec_weights)
   return(model)
 }
 
